a76fe9e9540104eb68a11f60ae36be1188cde61c
Yaroslav Yakushev <yyakushev@outlook.com>
2018-05-07 15:42:01 +0200

    comments added

--- CRMPerformanceTest/CRMPerformanceTest.ps1
+++ CRMPerformanceTest/CRMPerformanceTest.ps1
@@ -17,12 +17,13 @@ Param (
 	[Parameter(Mandatory=$false, ParameterSetName="sql")][string] $DatabaseName,
 	[Parameter(Mandatory=$false, ParameterSetName="sql")][string] $TableName
 )
+
 $formatdate = get-date -Format "HH.mm.ss_dd.MM.yyyy"
 $LogName = "$($formatdate)_CRMPerformanceTest.log"
 $script:ErrorActionPreference = "Stop"
 $Log = "$LogPath\$LogName"
 
-#Messaging functions
+#function writes events to event log and shows information messages on console
 function Write-ErrorEventLog ([string] $msg, [string] $Log) {
 	$t = $host.ui.RawUI.ForegroundColor
 	$host.ui.RawUI.ForegroundColor = "Red"
@@ -37,6 +38,7 @@ function Write-ErrorEventLog ([string] $msg, [string] $Log) {
 	}
 }
 
+#function writes events to event log and shows error messages on console
 function Write-InformationEventLog ([string] $msg, [string] $Log) {
 	$t = $host.ui.RawUI.ForegroundColor
 	$host.ui.RawUI.ForegroundColor = "Yellow"
@@ -82,23 +84,25 @@ Invoke-RestMethod   -Headers @{Authorization =("Bearer "+ $Dyn365Connection.Orga
                         -Method Get
 #>
 
+#download data from the internet
 function xhrLoad ([string]$url) {
 	try {
-		$wc = New-Object System.Net.WebClient
-		$start_time = Get-Date
-		$rr = $wc.DownloadData($url)
+		$wc = New-Object System.Net.WebClient #define web client
+		$start_time = Get-Date                
+		$rr = $wc.DownloadData($url)          
 	} catch {Write-ErrorEventLog  "$url could not be downloaded"}
-	return @{downloadedContentLength = $rr.Length; downloadTime = (Get-Date).Subtract($start_time).TotalMilliSeconds}
+	return @{downloadedContentLength = $rr.Length; downloadTime = (Get-Date).Subtract($start_time).TotalMilliSeconds} #return bytes and download time
 }
 
+#Run test downloads. $baseurl - the dynamics 365 company url. 
 function runDownloadTest ($whatToDownload, $trialsToRun, $baseurl) {
 	$isAdaptiveRun = $whatToDownload
-	$testResults = @()
+	$testResults = @() #save downladed data
     $downloadedContentLength = 0
     $lastRunSpeed = 0
     $prevAdpSpeed = 0
 
-	for ($i = 0; $i -lt $trialsToRun;$i++) {
+	for ($i = 0; $i -lt $trialsToRun;$i++) { #find the best size for downloading (adaptation)
 		$url = ""
 		if ($whatToDownload -is [System.Array]) {
             foreach ($adptFile in $whatToDownload) {
@@ -122,72 +126,61 @@ function runDownloadTest ($whatToDownload, $trialsToRun, $baseurl) {
     [int]$maxDownloadTime = $testResults[0].downloadTime
 	[int]$minDownloadTime = $testResults[0].downloadTime
 
-    foreach ($testResult in $testResults) {
+    foreach ($testResult in $testResults) { #find the max/min download time adn speed
 		$maxDownloadSpeed = [math]::Max($maxDownloadSpeed, $testResult.downloadSpeed)
 		$minDownloadSpeed = [math]::Min($minDownloadSpeed, $testResult.downloadSpeed)
         $maxDownloadTime = [math]::Max($maxDownloadTime, $testResult.downloadTime)
 		$minDownloadTime = [math]::Min($minDownloadTime, $testResult.downloadTime)
 	}
 
-    return @{testResults = $testResults;`
+    return @{testResults = $testResults;` #return download speed and time
 			maxDownloadSpeed = $maxDownloadSpeed;`
 			minDownloadSpeed = $minDownloadSpeed;`
 			maxDownloadTime = $maxDownloadTime;`
 			minDownloadTime = $minDownloadTime}
 }
 
+#function to find bandwidth
 function runBandwidthTest ($baseurl){
-	$adaptionSchedule = @(@{ speed= 0; url= "/_static/Tools/Diagnostics/random100x100.jpg" }, @{ speed= .5; url= "/_static/Tools/Diagnostics/random350x350.jpg" }, @{ speed= 1; url= "/_static/Tools/Diagnostics/random750x750.jpg" }, @{ speed= 2; url= "/_static/Tools/Diagnostics/random1000x1000.jpg" }, @{ speed= 4; url= "/_static/Tools/Diagnostics/random1500x1500.jpg" })
+	$adaptionSchedule = @(@{ speed= 0; url= "/_static/Tools/Diagnostics/random100x100.jpg" },` #different size of downloaded data
+						@{ speed= .5; url= "/_static/Tools/Diagnostics/random350x350.jpg" },`  #to adapt speed to the current bandwidth
+						@{ speed= 1; url= "/_static/Tools/Diagnostics/random750x750.jpg" },`   	
+						@{ speed= 2; url= "/_static/Tools/Diagnostics/random1000x1000.jpg" },`
+						@{ speed= 4; url= "/_static/Tools/Diagnostics/random1500x1500.jpg" })
 	$trialsToRun = 10
 	$results = runDownloadTest $adaptionSchedule $trialsToRun $baseurl
 	$testResults = $results.testResults
 	$i = 0
-#	$txt = "=== Bandwidth Test Info === `r`n"
 	$avgDownloadSpeed = 0
-<#	foreach ($testResult in $testResults) {
-        $txt += "Run " + ($i++) + "`r`n"
-        $txt += "  Time: " + $testResult.downloadTime + " ms`r`n"
-        $txt += "  Blob Size: " + $testResult.downloadedContentLength + " bytes`r`n"
-        $txt += "  Speed: " + $testResult.downloadSpeed + " KB/sec`r`n"
-		$avgDownloadSpeed += $testResult.downloadSpeed
-	}#>
 	$avgDownloadSpeed = [math]::floor($avgDownloadSpeed / $testResults.length)
-#    $txt += "Max Download speed: " + $results.maxDownloadSpeed + " KB/sec `r`n"
-#    $txt += "Min Download speed: " + $results.minDownloadSpeed + " KB/sec `r`n"
-#    Write-Host $txt;#>
 	Write-InformationEventLog -msg "Bandwidth test has been finished. The avarage download speed is $avgDownloadSpeed KB, the max download speed is $maxDownloadSpeed KB/sec and the min download speed is $minDownloadSpeed KB/sec" -Log $Log
     return @{avgDownloadSpeed = $avgDownloadSpeed;maxDownloadSpeed = $results.maxDownloadSpeed;minDownloadSpeed = $results.minDownloadSpeed}
 }
 
+#function to find the latency
 function runLatencyTest ($baseurl) {
     $trialsToRun = 20
     $results = runDownloadTest "/_static/Tools/Diagnostics/smallfile.txt" $trialsToRun $baseurl
     $testResults = $results.testResults
-#	$txt = "=== Latency Test Info === `r`n"
-#    $txt += "Number of times run: " + $trialsToRun + "`r`n"
 	$avgDownloadTime = 0
 	$i=0
     foreach ($testResult in $testResults) {
-#        $txt += "Run " + ($i++) + " time: " + $testResult.downloadTime + " ms`r`n"
         $avgDownloadTime += $testResult.downloadTime
     }
     $avgDownloadTime = [math]::floor($avgDownloadTime / $testResults.length)
 	Write-InformationEventLog -msg "Latency test has been finished. The avarage download time is $avgDownloadTime ms." -Log $Log
-#    $txt += "Max Download time: " + $results.maxDownloadTime + " ms `r`n"
-#    $txt += "Min Download time: " + $results.minDownloadTime + " ms `r`n"
-#    $txt += "Average latency: " + $avgDownloadTime + " ms`r`n"
-#    Write-Host $txt
 	return @{avgDownloadTime = $avgDownloadTime;maxDownloadTime = $results.maxDownloadTime;minDownloadTime = $results.minDownloadTime}
 }
 
+#function to run sql command
 function Invoke-SqlCommand {
 	Param (
-        [Parameter(Mandatory=$true)][Alias("Serverinstance")][string]$Server,
+        [Parameter(Mandatory=$true)][Alias("Serverinstance")][string]$Server, #server name like sqlserver\instancename
         [Parameter(Mandatory=$true)][string]$Database,
         [Parameter(Mandatory=$true, ParameterSetName="not_integrated")][string]$Username,
         [Parameter(Mandatory=$true, ParameterSetName="not_integrated")][string]$Password,
         [Parameter(Mandatory=$false, ParameterSetName="integrated")][switch]$UseWindowsAuthentication = $true,
-        [Parameter(Mandatory=$true)][string]$Query,
+        [Parameter(Mandatory=$true)][string]$Query, #sql command like 'Select * from [tablename]'
         [Parameter(Mandatory=$false)][int]$CommandTimeout=0
     )
     
@@ -222,6 +215,7 @@ function Invoke-SqlCommand {
 $BandwidthTest = runBandwidthTest $Dyn365Url
 $LatencyTest = runLatencyTest $Dyn365Url
 
+#creation of the insert sql query
 $query = "INSERT INTO [dbo].[$TableName] ([DateTime],`
 				[AvgBandwidth],`
 				[AvgLatency],`
